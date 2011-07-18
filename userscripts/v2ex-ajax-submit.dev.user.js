e3a30a7351907681a75137061da8066cba908907
Wilson <yyfearth@gmail.com>
2011-07-18 11:03:40 +0800

    upd ajax send for 1st reply

--- userscripts/v2ex-ajax-submit.dev.user.js
+++ userscripts/v2ex-ajax-submit.dev.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name ajax submit in V2EX
+// @name Ajax Send in V2EX
 // @description using ajax to submit in v2ex.
-// @version 0.1.1.1
+// @version 0.1.2.1
 // @auther yyfearth@gmail.com
-// @include *://*.v2ex.com/t/*
+// @include *://www.v2ex.com/t/*
 // @include *://v2ex.appspot.com/t/*
-// @match *://*.v2ex.com/t/*
+// @match *://www.v2ex.com/t/*
 // @match *://v2ex.appspot.com/t/*
 // ==/UserScript==
-(function () {
+(function() {
 	var txt = $('#reply_content'),
 	frm = txt.parents('form'),
 	sub = frm.find(':submit'),
@@ -22,55 +22,51 @@
 		if (!content) {
 			$('#e').html('Reply content cannot be empty!');
 			return false;
-		}
-		sub.val('Sending...').add(txt).attr('disabled', 'disabled');
-		//alert(url + '\n' + data);
-		$.ajax({
-			type: 'POST',
-			url: url,
-			dataType: 'html',
-			data: data,
-			success: function(html) {
-				if (html) {
-					//alert(html);
-					var rep = $('#replies'),
-					html = $(html.replace(/<script.*?<\/script>/ig,'')).find('#Content'),
-					rep_html = html.find('#replies').html();
-					if (!rep_html) { // no reps return
-						alert('err: no replies in return html');
-					} else if (!rep.length) { // no reply yet, use content instead
-						//$('#Content').html(html.html());
-						//J(); // rebind
+		} else if (!$('#replies').length) {
+			return true; // no reply yet, no ajax
+		} else {
+			sub.val('Sending...').add(txt).attr('disabled', 'disabled');
+			$.ajax({
+				type: 'POST',
+				url: url,
+				dataType: 'html',
+				data: data,
+				success: function(html) {
+					if (html) {
+						//alert(html);
+						var rep = $('#replies'),
+						rep_html = $(html.replace(/<script.*?<\/script>/ig, '')).find('#replies').html();
+						if (!rep_html) { // no reps return
+							alert('err: no replies in return html');
+						} else { // ok
+							rep.html(rep_html);
+						}
+					} else {
+						alert('err: ajax return empty');
 						location.reload();
-					} else { // ok
-						rep.html(rep_html);
 					}
-				} else {
-					alert('err: ajax return empty');
-					location.reload();
+				},
+				error: function(xhr) {
+					alert('err: ajax faild ' + xhr.status + ' - ' + xhr.responseText);
+				},
+				complete: function() { // after success or error
+					txt.val('').focus();
+					sub.val(subtxt).add(txt).removeAttr('disabled');
 				}
-			},
-			error: function(xhr) {
-				alert('err: ajax faild ' + xhr.status + ' - ' + xhr.responseText);
-			},
-			complete: function() {
-				txt.val('').focus();
-				sub.val(subtxt).add(txt).removeAttr('disabled');
-			}
-		});
-		return false;
+			});
+			return false;
+		}
 	});
-	txt.keydown(function(e) {
+	txt.keydown(function(e) { // ctrl+enter to send
 		if (e.ctrlKey && e.which == 13) {
 			e.stopPropagation();
 			frm.submit();
 			return false;
 		}
-	}).blur(function(){
+	}).blur(function() { // auto trim
 		txt.val($.trim(txt.val()));
 	});
 	sub.after('<span class="fade"> ctrl+enter </span>');
-	console.log('v2ex-ajax-submit-enabled');
 })();
 
 //(function(){var a=document.createElement("script");a.setAttribute("type","text/javascript");a.innerHTML='';document.getElementsByTagName("body")[0].appendChild(a)})();
--- userscripts/v2ex-ajax-submit.user.js
+++ userscripts/v2ex-ajax-submit.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name ajax submit in V2EX
+// @name Ajax Send in V2EX
 // @description using ajax to submit in v2ex.
-// @version 0.1.2.0
+// @version 0.1.2.1
 // @auther yyfearth@gmail.com
-// @match http://*.v2ex.com/t/*
-// @match https://*.v2ex.com/t/*
-// @match http://v2ex.appspot.com/t/*
-// @match https://v2ex.appspot.com/t/*
+// @include *://www.v2ex.com/t/*
+// @include *://v2ex.appspot.com/t/*
+// @match *://www.v2ex.com/t/*
+// @match *://v2ex.appspot.com/t/*
 // ==/UserScript==
-(function(){var a=document.createElement("script");a.setAttribute("type","text/javascript");a.innerHTML='(function(){var a=$("#reply_content"),b=a.parents("form"),d=b.find(":submit"),e=d.val();b.length>1&&(b=$(b[0]));b.submit(function(){var f=$.trim(a.val()),g=b.attr("action"),h=b.serialize();if(!f)return $("#e").html("Reply content cannot be empty!"),!1;d.val("Sending...").add(a).attr("disabled","disabled");$.ajax({type:"POST",url:g,dataType:"html",data:h,success:function(c){if(c){var a=$("#replies"),c=$(c.replace(/<script.*?<\\/script>/ig,"")).find("#Content");(c=c.find("#replies").html())?a.length?a.html(c):location.reload():alert("err: no replies in return html")}else alert("err: ajax return empty"),location.reload()},error:function(a){alert("err: ajax faild "+a.status+" - "+a.responseText)},complete:function(){a.val("").focus();d.val(e).add(a).removeAttr("disabled")}});return!1});a.keydown(function(a){if(a.ctrlKey&&a.which==13)return a.stopPropagation(),b.submit(),!1}).blur(function(){a.val($.trim(a.val()))});d.after(\'<span class="fade"> ctrl+enter </span>\');console.log("v2ex-ajax-submit-enabled")})()';document.getElementsByTagName("body")[0].appendChild(a)})();
+(function(){var a=document.createElement("script");a.setAttribute("type","text/javascript");a.innerHTML='(function(){var a=$("#reply_content"),b=a.parents("form"),c=b.find(":submit"),d=c.val();b.length>1&&(b=$(b[0]));b.submit(function(){var e=$.trim(a.val()),f=b.attr("action"),g=b.serialize();return e?$("#replies").length?(c.val("Sending...").add(a).attr("disabled","disabled"),$.ajax({type:"POST",url:f,dataType:"html",data:g,success:function(a){if(a){var b=$("#replies");(a=$(a.replace(/<script.*?<\\/script>/ig,"")).find("#replies").html())?b.html(a):alert("err: no replies in return html")}else alert("err: ajax return empty"),location.reload()},error:function(a){alert("err: ajax faild "+a.status+" - "+a.responseText)},complete:function(){a.val("").focus();c.val(d).add(a).removeAttr("disabled")}}),!1):!0:($("#e").html("Reply content cannot be empty!"),!1)});a.keydown(function(a){if(a.ctrlKey&&a.which==13)return a.stopPropagation(),b.submit(),!1}).blur(function(){a.val($.trim(a.val()))});c.after(\'<span class="fade"> ctrl+enter </span>\')})()';document.getElementsByTagName("body")[0].appendChild(a)})();
