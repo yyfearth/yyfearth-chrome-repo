bcdb49dc549add25a189bc06a1e3cb906704de78
Wilson Young <yyfearth@gmail.com>
2012-04-25 04:43:00 -0700

    support pb3 and cmd key

--- userscripts/v2ex-ajax-submit.dev.user.js
+++ userscripts/v2ex-ajax-submit.dev.user.js
@@ -1,29 +1,30 @@
 // ==UserScript==
 // @name Ajax Send in V2EX
 // @description using ajax to submit in v2ex.
-// @version 0.1.2.1
+// @version 0.1.3
 // @auther yyfearth@gmail.com
-// @include *://www.v2ex.com/t/*
-// @include *://v2ex.appspot.com/t/*
-// @match *://www.v2ex.com/t/*
-// @match *://v2ex.appspot.com/t/*
+// @include *://*.v2ex.com/t/*
+// @match *://*.v2ex.com/t/*
 // ==/UserScript==
 // ! this src file is only for dev, it will not have any effect yet!
-(function() {
+(function($) {
 	var txt = $('#reply_content'),
 	frm = txt.parents('form'),
 	sub = frm.find(':submit'),
-	subtxt = sub.val();
+	subtxt = sub.val(),
+	query = '#Main>.box:nth(1)',
+	regex = /感谢回复者|目前尚无回复|\d+\s*回复\s*\|\s*直到/;
 	if (frm.length > 1) frm = $(frm[0]);
 	frm.submit(function() {
 		//alert('sb');
 		var content = $.trim(txt.val()),
 		url = frm.attr('action'),
-		data = frm.serialize();
+		data = frm.serialize(),
+		replies = $(query);
 		if (!content) {
 			$('#e').html('Reply content cannot be empty!');
 			return false;
-		} else if (!$('#replies').length) {
+		} else if (!(replies.length && regex.test(replies.text()))) {
 			return true; // no reply yet, no ajax
 		} else {
 			sub.val('Sending...').add(txt).attr('disabled', 'disabled');
@@ -32,15 +33,21 @@
 				url: url,
 				dataType: 'html',
 				data: data,
+				timeout: 5000,
 				success: function(html) {
 					if (html) {
 						//alert(html);
-						var rep = $('#replies'),
-						rep_html = $(html.replace(/<script.*?<\/script>/ig, '')).find('#replies').html();
-						if (!rep_html) { // no reps return
+						html = $(html.replace(/<script.*?<\/script>/ig, ''));
+						if (!html) { // no reps return
 							alert('err: no replies in return html');
 						} else { // ok
-							rep.html(rep_html);
+							var reps = $(query, html);
+							if (regex.test(reps.text())) {
+								replies.replaceWith(reps);
+							} else {
+								alert('err: failed to parse responce!');
+								location.reload();
+							}
 						}
 					} else {
 						alert('err: ajax return empty');
@@ -59,7 +66,7 @@
 		}
 	});
 	txt.keydown(function(e) { // ctrl+enter to send
-		if (e.ctrlKey && e.which == 13) {
+		if ((e.ctrlKey || e.metaKey) && e.which == 13) {
 			e.stopPropagation();
 			frm.submit();
 			return false;
@@ -68,6 +75,6 @@
 		txt.val($.trim(txt.val()));
 	});
 	sub.after('<span class="fade"> ctrl+enter </span>');
-})();
+})(jQuery);
 
-//(function(){var a=document.createElement("script");a.setAttribute("type","text/javascript");a.innerHTML='';document.getElementsByTagName("body")[0].appendChild(a)})();
+//'(function(a){var c=a("#reply_content"),d=c.parents("form"),e=d.find(":submit"),h=e.val(),g=/\\u611f\\u8c22\\u56de\\u590d\\u8005|\\u76ee\\u524d\\u5c1a\\u65e0\\u56de\\u590d|\\d+\\s*\\u56de\\u590d\\s*\\|\\s*\\u76f4\\u5230/;1<d.length&&(d=a(d[0]));d.submit(function(){var i=a.trim(c.val()),j=d.attr("action"),k=d.serialize(),f=a("#Main>.box:nth(1)");if(i){if(!f.length||!g.test(f.text()))return!0;e.val("Sending...").add(c).attr("disabled","disabled");a.ajax({type:"POST",url:j,dataType:"html",data:k,timeout:5E3,success:function(b){b?(b=a(b.replace(/<script.*?<\\/script>/ig,"")))?(b=a("#Main>.box:nth(1)",b),g.test(b.text())?f.replaceWith(b):(alert("err: failed to parse responce!"),location.reload())):alert("err: no replies in return html"):(alert("err: ajax return empty"),location.reload())},error:function(b){alert("err: ajax faild "+b.status+" - "+b.responseText)},complete:function(){c.val("").focus();e.val(h).add(c).removeAttr("disabled")}});return!1}a("#e").html("Reply content cannot be empty!");return!1});c.keydown(function(a){if((a.ctrlKey||a.metaKey)&&13==a.which)return a.stopPropagation(),d.submit(),!1}).blur(function(){c.val(a.trim(c.val()))});e.after(\'<span class="fade"> ctrl+enter </span>\')})(jQuery);'
--- userscripts/v2ex-ajax-submit.user.js
+++ userscripts/v2ex-ajax-submit.user.js
@@ -1,11 +1,9 @@
 // ==UserScript==
 // @name Ajax Send in V2EX
 // @description using ajax to submit in v2ex.
-// @version 0.1.2.1
+// @version 0.1.3
 // @auther yyfearth@gmail.com
-// @include *://www.v2ex.com/t/*
-// @include *://v2ex.appspot.com/t/*
-// @match *://www.v2ex.com/t/*
-// @match *://v2ex.appspot.com/t/*
+// @include *://*.v2ex.com/t/*
+// @match *://*.v2ex.com/t/*
 // ==/UserScript==
-(function(){var a=document.createElement("script");a.setAttribute("type","text/javascript");a.innerHTML='(function(){var a=$("#reply_content"),b=a.parents("form"),c=b.find(":submit"),d=c.val();b.length>1&&(b=$(b[0]));b.submit(function(){var e=$.trim(a.val()),f=b.attr("action"),g=b.serialize();return e?$("#replies").length?(c.val("Sending...").add(a).attr("disabled","disabled"),$.ajax({type:"POST",url:f,dataType:"html",data:g,success:function(a){if(a){var b=$("#replies");(a=$(a.replace(/<script.*?<\\/script>/ig,"")).find("#replies").html())?b.html(a):alert("err: no replies in return html")}else alert("err: ajax return empty"),location.reload()},error:function(a){alert("err: ajax faild "+a.status+" - "+a.responseText)},complete:function(){a.val("").focus();c.val(d).add(a).removeAttr("disabled")}}),!1):!0:($("#e").html("Reply content cannot be empty!"),!1)});a.keydown(function(a){if(a.ctrlKey&&a.which==13)return a.stopPropagation(),b.submit(),!1}).blur(function(){a.val($.trim(a.val()))});c.after(\'<span class="fade"> ctrl+enter </span>\')})()';document.getElementsByTagName("body")[0].appendChild(a)})();
+(function(){var js='(function(a){var c=a("#reply_content"),d=c.parents("form"),e=d.find(":submit"),h=e.val(),g=/\\u611f\\u8c22\\u56de\\u590d\\u8005|\\u76ee\\u524d\\u5c1a\\u65e0\\u56de\\u590d|\\d+\\s*\\u56de\\u590d\\s*\\|\\s*\\u76f4\\u5230/;1<d.length&&(d=a(d[0]));d.submit(function(){var i=a.trim(c.val()),j=d.attr("action"),k=d.serialize(),f=a("#Main>.box:nth(1)");if(i){if(!f.length||!g.test(f.text()))return!0;e.val("Sending...").add(c).attr("disabled","disabled");a.ajax({type:"POST",url:j,dataType:"html",data:k,timeout:5E3,success:function(b){b?(b=a(b.replace(/<script.*?<\\/script>/ig,"")))?(b=a("#Main>.box:nth(1)",b),g.test(b.text())?f.replaceWith(b):(alert("err: failed to parse responce!"),location.reload())):alert("err: no replies in return html"):(alert("err: ajax return empty"),location.reload())},error:function(b){alert("err: ajax faild "+b.status+" - "+b.responseText)},complete:function(){c.val("").focus();e.val(h).add(c).removeAttr("disabled")}});return!1}a("#e").html("Reply content cannot be empty!");return!1});c.keydown(function(a){if((a.ctrlKey||a.metaKey)&&13==a.which)return a.stopPropagation(),d.submit(),!1}).blur(function(){c.val(a.trim(c.val()))});e.after(\'<span class="fade"> ctrl+enter </span>\')})(jQuery);',a=document.createElement("script");a.setAttribute("type","text/javascript");a.innerHTML=js;document.getElementsByTagName("body")[0].appendChild(a)})();
